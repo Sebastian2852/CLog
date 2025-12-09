d55d5da5304a306ed3ee578d05bb1f96e9921dc8
sebastian2852 <sebastian2852@proton.me>
2025-12-09 01:17:46 +0000

    Expanded examples to show off custom formatting

--- example/main.cpp
+++ example/main.cpp
@@ -2,12 +2,27 @@
 
 int main()
 {
-	CLog::Logger myLogger("CoolLogger");
-	myLogger.AddSink<CLog::Sink::ConsoleSink>();
-	myLogger.AddSink<CLog::Sink::FileSink>("Debug.log");
-	myLogger.Trace("Hello World!");
-	myLogger.Debug("Hello World!");
-	myLogger.Info("Hello World!");
-	myLogger.Warn("Hello World!");
-	myLogger.Error("Hello World!");
+	// Basic logger
+	{
+		CLog::Logger myLogger("Basic Logger");
+		myLogger.AddSink<CLog::Sink::ConsoleSink>();
+		myLogger.AddSink<CLog::Sink::FileSink>("BasicLogger.log");
+		myLogger.Trace("Hello World!");
+		myLogger.Debug("Hello World!");
+		myLogger.Info("Hello World!");
+		myLogger.Warn("Hello World!");
+		myLogger.Error("Hello World!");
+	}
+
+	// Custom formatted logger
+	{
+		CLog::Logger myLogger("Custom Format Logger", "%{NAME} - %{PREFIX}: %{COLOR_START}%{MSG}");
+		myLogger.AddSink<CLog::Sink::ConsoleSink>();
+		myLogger.AddSink<CLog::Sink::FileSink>("CustomFormattedLogger.log");
+		myLogger.Trace("Hello World!");
+		myLogger.Debug("Hello World!");
+		myLogger.Info("Hello World!");
+		myLogger.Warn("Hello World!");
+		myLogger.Error("Hello World!");
+	}
 }
